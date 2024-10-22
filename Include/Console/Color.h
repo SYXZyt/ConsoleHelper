5b285e27875a6b1bfb28b547cb5ec328697614f2
Jake Milner <49202777+SYXZyt@users.noreply.github.com>
2024-10-22 18:07:06 +0100

    Fixed codes on terminals without ANSI support

--- Include/Console/Color.h
+++ Include/Console/Color.h
@@ -1,62 +1,81 @@
 #pragma once
 #include <string>
 
+#ifdef _WIN32
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <Windows.h>
+#endif
+
 namespace Console
 {
-    enum class Color
-    {
-        BLACK,
-        BLUE,
-        GREEN,
-        AQUA,
-        RED,
-        PURPLE,
-        YELLOW,
-        WHITE,
-        GREY,
+	enum class Color
+	{
+		BLACK,
+		BLUE,
+		GREEN,
+		AQUA,
+		RED,
+		PURPLE,
+		YELLOW,
+		WHITE,
+		GREY,
 
-        LIGHT_BLUE,
-        LIGHT_GREEN,
-        LIGHT_AQUA,
-        LIGHT_RED,
-        LIGHT_PURPLE,
-        LIGHT_YELLOW,
-        BRIGHT_WHITE,
+		LIGHT_BLUE,
+		LIGHT_GREEN,
+		LIGHT_AQUA,
+		LIGHT_RED,
+		LIGHT_PURPLE,
+		LIGHT_YELLOW,
+		BRIGHT_WHITE,
 
-        Colour_Size,
-    };
+		Colour_Size,
+	};
 
-    using Colour = Color;
+	using Colour = Color;
 
-    inline std::string GetAnsiEscapeSequence(Color colour, bool isForeground)
-    {
-        const std::string foregroundBase = "\033[3";
-        const std::string backgroundBase = "\033[4";
+	inline std::string GetAnsiEscapeSequence(Color colour, bool isForeground)
+	{
+#ifdef _WIN32
+		//Older terminals on Windows don't support ANSI codes, so if this terminal does not support then
+		//Then do not print anything
+		HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
 
-        std::string code;
-        switch (colour)
-        {
-            case Color::BLACK:         code = "0"; break;
-            case Color::RED:           code = "1"; break;
-            case Color::GREEN:         code = "2"; break;
-            case Color::YELLOW:        code = "3"; break;
-            case Color::BLUE:          code = "4"; break;
-            case Color::PURPLE:        code = "5"; break;
-            case Color::AQUA:          code = "6"; break;
-            case Color::WHITE:         code = "7"; break;
-            case Color::GREY:          code = "8"; break;
+		DWORD dwMode = 0;
+		GetConsoleMode(hOut, &dwMode);
 
-            case Color::LIGHT_RED:     code = "9"; break;
-            case Color::LIGHT_GREEN:   code = "10"; break;
-            case Color::LIGHT_YELLOW:  code = "11"; break;
-            case Color::LIGHT_BLUE:    code = "12"; break;
-            case Color::LIGHT_PURPLE:  code = "13"; break;
-            case Color::LIGHT_AQUA:    code = "14"; break;
-            case Color::BRIGHT_WHITE:  code = "15"; break;
+		if (!(dwMode & ENABLE_VIRTUAL_TERMINAL_PROCESSING))
+			return "";
+#endif
 
-            default:                    code = "0"; break;
-        }
+		const std::string foregroundBase = "\033[3";
+		const std::string backgroundBase = "\033[4";
 
-        return (isForeground ? foregroundBase : backgroundBase) + code + "m";
-    }
+		std::string code;
+		switch (colour)
+		{
+			case Color::BLACK:         code = "0"; break;
+			case Color::RED:           code = "1"; break;
+			case Color::GREEN:         code = "2"; break;
+			case Color::YELLOW:        code = "3"; break;
+			case Color::BLUE:          code = "4"; break;
+			case Color::PURPLE:        code = "5"; break;
+			case Color::AQUA:          code = "6"; break;
+			case Color::WHITE:         code = "7"; break;
+			case Color::GREY:          code = "8"; break;
+
+			case Color::LIGHT_RED:     code = "9"; break;
+			case Color::LIGHT_GREEN:   code = "10"; break;
+			case Color::LIGHT_YELLOW:  code = "11"; break;
+			case Color::LIGHT_BLUE:    code = "12"; break;
+			case Color::LIGHT_PURPLE:  code = "13"; break;
+			case Color::LIGHT_AQUA:    code = "14"; break;
+			case Color::BRIGHT_WHITE:  code = "15"; break;
+
+			default:                    code = "0"; break;
+		}
+
+		return (isForeground ? foregroundBase : backgroundBase) + code + "m";
+	}
 }
